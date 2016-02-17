254d78a5396cabc37b05e211831d2824f3dc7b7a
Alejandro Paiz <alejandro.paiz@entrepids.com>
2016-02-17 12:26:42 -0300

    Se agrega el método printDebugBacktrace()

--- magento-u-vm/magento/htdocs/app/Mage.php
+++ magento-u-vm/magento/htdocs/app/Mage.php
@@ -987,4 +987,24 @@ final class Mage
     {
         self::$_isDownloader = $flag;
     }
+
+    public static function printDebugBacktrace($title = 'Debug Backtrace:') {
+    	$output     = "";
+    	$output .= "<hr /><div>" . $title . '<br /><table border="1" cellpadding="2" cellspacing="2">';
+
+    	$stacks     = debug_backtrace();
+
+    	$output .= "<thead><tr><th><strong>File</strong></th><th><strong>Line</strong></th><th><strong>Function</strong></th>".
+    			"</tr></thead>";
+    	foreach($stacks as $_stack)
+    	{
+    		if (!isset($_stack['file'])) $_stack['file'] = '[PHP Kernel]';
+    		if (!isset($_stack['line'])) $_stack['line'] = '';
+
+    		$output .=  "<tr><td>{$_stack["file"]}</td><td>{$_stack["line"]}</td>".
+    				"<td>{$_stack["function"]}</td></tr>";
+    	}
+    	$output .=  "</table></div><hr /></p>";
+    	return $output;
+    }
 }
